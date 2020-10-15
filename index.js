9186e091bc99c46eef5b9202dd0bbb23f4419566
bariisarslans <bariisarslans@hotmail.com>
2020-10-15 10:45:28 +0300

    1.0.28

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function checkStatus(response) {
 
 var create_api = function(organizationID, siteID, segmentURL, dataSource, realTimeURL, channel, euroMsgApplicationKey, euroMsgSubscriptionURL, euroMsgRetentionURL, local) {
 
-	const sdkVersion = "1.0.27";
+	const sdkVersion = "1.0.28";
 	const euroSubscriptionKey = "subscription";
 
 	let runController = 0;
